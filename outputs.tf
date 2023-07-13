ae9936bc97e0c68071b7197e9ebc08f971b02250
xat <descamps.jeremy@gmail.com>
2023-07-13 13:00:33 +0200

    hotfix(outputs): arrays output values linting

--- outputs.tf
+++ outputs.tf
@@ -9,10 +9,10 @@ output "resource_share_id" {
 
 # RAM Principal Association outputs
 output "principal_ids" {
-  value = [ for pid in aws_ram_principal_association.this: pid.id ]
+  value = [for pid in aws_ram_principal_association.this : pid.id]
 }
 
 # RAM Resource Association outputs
 output "resource_ids" {
-  value = [ for resource_id in aws_ram_resource_association.this: resource_id.id ]
+  value = [for resource_id in aws_ram_resource_association.this : resource_id.id]
 }
